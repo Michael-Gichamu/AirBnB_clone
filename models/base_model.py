2681892da61f5e2f5f2d147047cf9c9e4aea5163
Peter Evance <peterevance1@gmail.com>
2023-05-10 21:23:41 +0300

    Included missing `__init__` python package identifiers and introduced more projects base folder structure

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
 """
-Defines a base model class.
+BaseModel class definition
 """
+
+import uuid
 from datetime import datetime
-from uuid import uuid4
 
 
 class BaseModel:
     """
-    Base Class for all models.
+    The BaseModel class from which future classes will be inherited from
     """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
-    def __init__(self, *args, **kwargs):
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel class
+        """
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
-        Updates public instance attribute updated_at
-        with current datetime
+        Updates the attribute `updated_at` with the current datetime
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values.
-        """
-        instance_dict = self.__dict__.copy()
-        instance_dict['__class__'] = self.__class__.__name__
-        instance_dict['created_at'] = self.created_at.isoformat()
-        instance_dict['updated_at'] = self.updated_at.isoformat()
-        return instance_dict
-
-    def __str__(self):
-        """
-        Return the str() representation of the Basemodel.
-        """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
