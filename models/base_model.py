4b859af78ecc6fe050012a74d20fe2345a08b337
Michael-Gichamu <michaelmatere606@gmail.com>
2023-05-11 03:28:27 -0700

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 """
-BaseModel class definition
+Defines BaseModel Class.
 """
-
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """
     The BaseModel class from which future classes will be inherited from
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization of a Base instance.
 
-    def __str__(self):
+        Args:
+            args: list of arguemnts.
+            kwargs: dict of key-value arguments.
         """
-        Returns a string representation of the BaseModel class
-        """
-        class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+        if kwargs == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            Tformat = "%Y-%m-%dT%H:%M:%S.%f"
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], Tformat)
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], Tformat)
+            for key, value in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, value)
 
     def save(self):
         """
@@ -30,8 +38,20 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary that contains all key/values
+        of the instance.
+
+        """
         data = self.__dict__.copy()
         data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
         data['updated_at'] = self.updated_at.isoformat()
         return data
+
+    def __str__(self):
+        """
+        Return the string representation of the Basemodel.
+        """
+        return f "[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
