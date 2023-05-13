3f046fc2cdb5602398d6357ebd1476235ebe2db4
Peter Evance <peterevance1@gmail.com>
2023-05-13 19:47:30 +0300

    `Users` class introduced - users.py

---
+++ models/user.py
@@ -0,0 +1,11 @@
+from .base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class
+    """
+    email: str = ""
+    password: str = ""
+    first_name: str = ""
+    last_name: str = ""
