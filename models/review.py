d020c6b2ea98c9664bc35469a450fb835e3a8ede
Peter Evance <peterevance1@gmail.com>
2023-05-13 19:54:43 +0300

    `Review` class introduced - review.py

---
+++ models/review.py
@@ -0,0 +1,10 @@
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Review class that inherits from BaseModel.
+    """
+    place_id: str = ""
+    user_id: str = ""
+    text: str = ""
